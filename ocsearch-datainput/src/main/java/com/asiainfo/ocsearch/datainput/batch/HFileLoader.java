3e1290f182e61881a119c4add9fac59e7c7a1ab8
王征 <wangzheng10@asiainfo.com>
2017-05-31 11:49:04 +0800

    update pom

--- ocsearch-datainput/src/main/java/com/asiainfo/ocsearch/datainput/batch/HFileLoader.java
+++ ocsearch-datainput/src/main/java/com/asiainfo/ocsearch/datainput/batch/HFileLoader.java
@@ -3,5 +3,24 @@ package com.asiainfo.ocsearch.datainput.batch;
 /**
  * Created by Aaron on 17/5/26.
  */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles;
+
 public class HFileLoader {
+    public static void doBulkLoad(String pathToHFile, String tableName){
+        try {
+            Configuration configuration = new Configuration();
+            HBaseConfiguration.addHbaseResources(configuration);
+            LoadIncrementalHFiles loadFfiles = new LoadIncrementalHFiles(configuration);
+            HTable hTable = new HTable(configuration, tableName);//指定表名
+            loadFfiles.doBulkLoad(new Path(pathToHFile), hTable);//导入数据
+            System.out.println("Bulk Load Completed..");
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
 }
